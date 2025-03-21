626b1a8628cb99a619c86ce8c9bfa6c3cc6d38f7
Leslie Petrimaux <leslypetrimaux@gmail.com>
2025-03-21 12:22:35 +0100

    Prepare new `Kernel`

--- src/Interface/KernelInterface.php
+++ src/Interface/KernelInterface.php
@@ -4,11 +4,5 @@ namespace Eightyfour\Interface;
 
 interface KernelInterface
 {
-    function run(): void;
 
-    function fromRequest(): void;
-
-    function fromCli(): void;
-
-    function toResponse(): void;
 }
