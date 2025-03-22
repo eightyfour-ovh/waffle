4543f1fe69f7294af4fd92c624bf151a6625aa22
Leslie Petrimaux <leslypetrimaux@gmail.com>
2025-03-22 04:17:25 +0100

    test `Kernel`

--- tests/src/KernelTest.php
+++ tests/src/KernelTest.php
@@ -2,7 +2,11 @@
 
 namespace EightyfourTests;
 
+use Eightyfour\Core\Cli;
+use Eightyfour\Core\Request;
+use Eightyfour\Core\Response;
 use Eightyfour\Kernel;
+use PHPUnit\Framework\MockObject\Exception;
 use PHPUnit\Framework\TestCase;
 
 class KernelTest extends TestCase
@@ -17,6 +21,80 @@ class KernelTest extends TestCase
 
         // Expects
         $this->assertTrue($result);
+    }
+
+    public function testCreateRequestFromGlobals(): void
+    {
+        // Given
+        $class = $this->getClass();
+
+        // When
+        $result = $class->createRequestFromGlobals();
+
+        // Expects
+        $this->assertInstanceOf(expected: Request::class, actual: $result);
+    }
+
+    public function testCreateCli(): void
+    {
+        // Given
+        $class = $this->getClass();
+
+        // When
+        $result = $class->createCli();
+
+        // Expects
+        $this->assertInstanceOf(expected: Cli::class, actual: $result);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testRunRequest(): void
+    {
+        // Given
+        $class = $this->getClass();
+        $handler = $this->createMock(type: Request::class);
+        $response = $this->createMock(type: Response::class);
+
+        // Then
+        $response
+            ->expects($this->once())
+            ->method(constraint: 'render')
+        ;
+        $handler
+            ->expects($this->once())
+            ->method(constraint: 'process')
+            ->willReturn($response)
+        ;
+
+        // When
+        $class->run(handler: $handler);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testRunCli(): void
+    {
+        // Given
+        $class = $this->getClass();
+        $handler = $this->createMock(type: Cli::class);
+        $response = $this->createMock(type: Response::class);
+
+        // Then
+        $response
+            ->expects($this->once())
+            ->method(constraint: 'render')
+        ;
+        $handler
+            ->expects($this->once())
+            ->method(constraint: 'process')
+            ->willReturn($response)
+        ;
+
+        // When
+        $class->run(handler: $handler);
     }
 
     private function getClass(): Kernel
